fc2488a1d66a06a9fe8c83b38757a944b1bc49e0
Kevin Novak <knovak8@gmail.com>
2018-05-02 23:47:17 -0700

    Setting basics for service calls from client

--- static/client.js
+++ static/client.js
@@ -1,6 +1,13 @@
 $(function() {
 
     $('#search').click(() => {
-        alert('hihi')
+        // alert($('#customGames').val().split(', '));
+        // alert($('#gameList').val())
+
+        fetch('http://localhost:3000/api/games/top?first=5')
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+        })
     })
 })
