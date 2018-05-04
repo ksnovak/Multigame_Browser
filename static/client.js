e5fef9c691637a662aa08bb6d7de67915c50d73e
Kevin Novak <knovak8@gmail.com>
2018-05-04 01:36:54 -0700

    Reload upon search, with new game selection

--- static/client.js
+++ static/client.js
@@ -4,10 +4,29 @@ $(function() {
         // alert($('#customGames').val().split(', '));
         // alert($('#gameList').val())
 
-        fetch('http://localhost:3000/api/games/top?first=5')
-        .then(response => response.json())
-        .then(data => {
-            console.log(data);
-        })
+        updateGamesList( $('#customGames').val().split(', '), $('#gameList').val());
+        
     })
+
+    let updateGamesList = function(nameArray, idArray) {
+        $.get({
+            url: './api/games/specific',
+            data: { 
+                name: nameArray,
+                id: idArray
+            },
+            error: response => { console.log (`Error: ${response}`) },
+            success: response => {
+                let querystring = '?';
+                response.forEach(game => {
+                    querystring += `name=${game.name}&`;
+                });
+                window.location.href = window.location.protocol + "//" + window.location.host + window.location.pathname + querystring;
+            }
+        })
+    }
+
+    let setGame = function(game) {
+
+    }
 })
