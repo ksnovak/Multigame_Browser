9a2b3772798b23a162bf5d1a5aa385d4177e0488
Kevin Novak <knovak8@gmail.com>
2018-08-15 20:01:22 -0700

    Test commit for Travis
    Does this properly go through Travis?

--- src/server/index.js
+++ src/server/index.js
@@ -6,8 +6,6 @@ const app = express();
 
 const port = process.env.PORT || 8080;
 
-// console.log(process.env.NODE_ENV);
-
 app.use(express.static('dist'));
 app.get('/api/getUsername', (req, res) => res.send({ username: os.userInfo().username }));
 app.listen(port, () => console.log(`Listening on port ${port}!`));
