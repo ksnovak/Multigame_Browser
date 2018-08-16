8ebb5e2b9b6e676ef00c18ba11eb41124812cfa9
Kevin Novak <knovak8@gmail.com>
2018-08-15 17:06:52 -0700

    Changing heroku build process
    It needs to know to run webpack before trying to serve up content.

--- src/server/index.js
+++ src/server/index.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const os = require('os');
+require('dotenv').config();
 
 const app = express();
 
+const port = process.env.PORT || 8080;
+
+// console.log(process.env.NODE_ENV);
+
 app.use(express.static('dist'));
 app.get('/api/getUsername', (req, res) => res.send({ username: os.userInfo().username }));
-app.listen(8080, () => console.log('Listening on port 8080!'));
+app.listen(port, () => console.log(`Listening on port ${port}!`));
