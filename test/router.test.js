f0bbf551722598f091d4bcbaa55309fce42c5697
Kevin Novak <knovak8@gmail.com>
2018-08-22 23:25:00 -0700

    Establishing test files

---
+++ test/router.test.js
@@ -0,0 +1,15 @@
+import assert from 'assert';
+import chai from 'chai';
+
+const should = chai.should();
+
+describe('Router', () => {
+  describe('/api', () => {
+    describe('/games', () => {
+      it('exists');
+    });
+    describe('/streams', () => {
+      it('exists');
+    });
+  });
+});
