bd5afdab17c529f7b922176f733b4b218d0f3993
Kevin Novak <knovak8@gmail.com>
2018-08-26 21:09:54 -0700

    Cleaned up getting Streams
    Twitch API allows you to either specify game or user (if you do both, it acts like an inner join, which we don't want)
    So we have to add in a case to handle the user passing both -- by just making two separate Twitch calls and combining them.

--- src/server/Routes/streams.js
+++ src/server/Routes/streams.js
@@ -15,14 +15,45 @@ function streamsFromData(body) {
   }
 }
 
-async function getStreamsForGame(options, next) {
-  const results = await RoutesUtils.commonTwitchRequest({ uri: '/helix/streams', qs: options, rejectErrors: true, next });
+async function getStreams(options, next) {
+  //Twitch's API requires you to ask for either game ID(s) xor user detail(s). Doing both acts like an inner join, which is what we don't want.
+  //In this case, if the user wants both types, we'll make two calls and combine the results:
+  if (options.game_id && (options.user_id || options.user_login)) {
+    let gameOnlyOptions = Object.assign({}, options)
+    gameOnlyOptions.user_id = undefined;
+    gameOnlyOptions.user_login = undefined;
 
-  return streamsFromData(results);
+    let userOnlyOptions = Object.assign({}, options)
+    userOnlyOptions.game_id = undefined;
+
+    const [userStreams, gameStreams] = [await getStreams(userOnlyOptions, next), await getStreams(gameOnlyOptions, next)]
+
+    let combinedStreams = []
+    if (!userStreams.length || !gameStreams.length) {
+      combinedStreams = userStreams.length ? userStreams : gameStreams;
+    }
+    else {
+      combinedStreams = userStreams;
+
+      gameStreams.forEach(gameStream => {
+        let index = combinedStreams.map(userStream => { return userStream.user_id }).indexOf(gameStream.user_id)
+        if (index === -1) {
+          combinedStreams.push(gameStream)
+        }
+      })
+    }
+
+    return combinedStreams;
+  }
+  //If the user only requested one of the types, make a simpler request:
+  else {
+    const results = await RoutesUtils.commonTwitchRequest({ uri: '/helix/streams', qs: options, rejectErrors: true, next });
+
+    return streamsFromData(results);
+  }
 }
 
-/* Get list of live streams for specified games
-    Querystring params: game_id, language, first
+/* Get list of live streams, for either the specified game or specified users
     WARNING: You can specify either the game, or the streamer. If you do both, it returns an inner join basically (all of the specified people, streaming the specified games)
     https://dev.twitch.tv/docs/api/reference/#get-streams
 */
@@ -37,7 +68,7 @@ router.get('/list', async (req, res, next) => {
     res.send([]);
   } else {
     try {
-      const results = await getStreamsForGame(options, next);
+      const results = await getStreams(options, next);
       res.send(results);
     }
     catch (err) {
