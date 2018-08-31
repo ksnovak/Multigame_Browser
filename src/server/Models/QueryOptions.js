df8ea4ca3d47cd6ed110dc3d52a2aed38dcabd8e
Kevin Novak <knovak8@gmail.com>
2018-08-31 06:59:36 -0700

    Start rework of QueryOption parsing
    Taking a much more modular approach to this instead of having the behemoth function.

--- src/server/Models/QueryOptions.js
+++ src/server/Models/QueryOptions.js
@@ -37,8 +37,8 @@ const queryOptions = {
   '/combo': {
     includetop: { type: 'boolean', default: false },
     first: { type: 'number' },
-    id: { type: 'number', duplicate: true },  //game ID
-    name: { duplicate: true },                //game name
+    id: { type: 'number', duplicate: true }, // game ID
+    name: { duplicate: true }, // game name
     game_id: { type: 'number', duplicate: true },
     language: { duplicate: true },
     user_id: { type: 'number', duplicate: true },
@@ -46,7 +46,147 @@ const queryOptions = {
   }
 };
 
+// All options from the user that we accept
+const incomingOptions = {
+  '/games/top': {
+    games_count: { type: 'number', default: 30, duplicate: true },
+    games_before: { type: 'number' },
+    games_after: { type: 'number' },
+    include_top: { type: 'boolean' }
+  },
+  '/games/specific': {
+    game_id: { type: 'number', duplicate: true },
+    game_name: { duplicate: true }
+  },
+  '/games/combo': {
+    include_top_games: { type: 'boolean', default: false },
+    games_count: { type: 'number' },
+    game_id: { type: 'number', duplicate: true },
+    game_name: { duplicate: true }
+  },
+  '/streams/list': {
+    streams_count: { type: 'number' },
+    streams_before: { type: 'number' },
+    streams_after: { type: 'number' },
+    game_id: { type: 'number', duplicate: true },
+    stream_id: { type: 'number', duplicate: true },
+    stream_name: { duplicate: true },
+    language: { duplicate: true }
+  },
+  '/streams/top': {
+    streams_count: { type: 'number' },
+    streams_before: { type: 'number' },
+    streams_after: { type: 'number' },
+    language: { duplicate: true }
+  },
+  '/combo': {
+    include_top_games: { type: 'boolean', default: false },
+    include_top_streams: { type: 'boolean', default: false },
+    games_count: { type: 'number' },
+    streams_count: { type: 'number' },
+    game_id: { type: 'number', duplicate: true }, // game ID
+    game_name: { duplicate: true }, // game name
+    stream_id: { type: 'number', duplicate: true },
+    stream_name: { duplicate: true },
+    language: { duplicate: true }
+  }
+};
+
 module.exports = {
+  // Get all of the possible options for a given endpoint
+  getOptionSet(endpoint) {
+    return incomingOptions[endpoint] || null;
+  },
+
+  // Make sure the passed-in parameter is even able to be valid
+  getCleanName(name) {
+    if (name.length === 0 || !isNaN(name)) {
+      return null;
+    }
+    return name.toLowerCase();
+  },
+
+  // For a given parameter, get all of the properties that it can have.
+  getAllowedProperties(optionSet, cleanName) {
+    return optionSet[cleanName] || null;
+  },
+
+  // With the user-sent params, get all of the parameter names that clean up to the specified one
+  getMatchingNames(params, optionName) {
+    const names = Object.keys(params).filter(
+      paramName => optionName === this.getCleanName(paramName)
+    );
+    return names.length ? names : null;
+  },
+
+  // Retrieve the user-sent values that match the specified parameter
+  getPassedVals(allowedProperties, passedNames, params) {
+    let values = [];
+    passedNames.forEach((name) => {
+      values = values.concat(params[name]);
+    });
+    return values.length ? values : null;
+  },
+
+  // In case the params have to be numbers, retrieve an array of only valid numbers from the given values
+  getCleanedNumbers(values) {
+    const cleanedNumbers = [];
+    values.forEach((val) => {
+      if (!isNaN(val) && val !== null) cleanedNumbers.push(Number(val));
+    });
+
+    return cleanedNumbers.length ? cleanedNumbers : null;
+  },
+
+  // In case the params have to be bool, retrieve an array of only valid booleans from the given values
+  getCleanedBools(values) {
+    const cleanedBools = [];
+    values.forEach((val) => {
+      if (typeof val === 'boolean') {
+        cleanedBools.push(val);
+      }
+      else if (typeof val === 'number' && (val === 0 || val === 1)) {
+        cleanedBools.push(!!val);
+      }
+      else if (typeof val === 'string') {
+        const lowerString = val.toLowerCase();
+
+        if (lowerString === 't' || lowerString === 'true') cleanedBools.push(true);
+        else if (lowerString === 'f' || lowerString === 'false') cleanedBools.push(false);
+      }
+    });
+
+    return cleanedBools.length ? cleanedBools : null;
+  },
+
+  // Check if there is a default value for a given parameter, and fall back to it if the parameter value currently is invalid
+  getDefaultValue(defaultVal, currentVal) {
+    if (currentVal !== undefined && currentVal !== null) {
+      return currentVal;
+    }
+
+    if (defaultVal !== undefined) {
+      return defaultVal;
+    }
+
+    return currentVal;
+  },
+
+  // Simplify the array if possible - if either there's only 1 element, or multiples are disallowed, return the first element
+  getSimplifiedArray(duplicatesAllowed, currentVal) {
+    if (Array.isArray(currentVal)) {
+      if (!duplicatesAllowed || currentVal.length === 1) {
+        return currentVal[0];
+      }
+      if (currentVal.length > 1) {
+        return currentVal;
+      }
+    }
+
+    return currentVal != null ? currentVal : null;
+  },
+
+  //
   // Return all valid and relevant querystring parameters, given the specified endpoint, and the starting querystring object.
   getValidQueryOptions(endpoint, params) {
     const allowedParams = queryOptions[endpoint]; // Based on the endpoint, get the list of allowed parameters
--- test/models.test.js
+++ test/models.test.js
@@ -1,5 +1,6 @@
 import assert from 'assert';
-import chai from 'chai';
+import chai, { expect } from 'chai';
+import QueryOptions from '../src/server/Models/QueryOptions';
 
 const should = chai.should();
 
@@ -14,6 +15,100 @@ describe('Models', () => {
     it('is error');
   });
   describe('QueryOptions', () => {
-    it('is query options');
+    describe('Option set', () => {
+      it('Gets a set of valid options when passed a valid endpoint', (done) => {
+        expect(QueryOptions.getOptionSet('/games/specific')).to.have.property('game_id');
+        done();
+      });
+
+      it('Gets no options when passed an invalid endpoint', (done) => {
+        expect(QueryOptions.getOptionSet('/blah/bloop')).to.equal(null);
+        expect(QueryOptions.getOptionSet(null)).to.equal(null);
+        done();
+      });
+    });
+
+    describe('Name cleaning', () => {
+      it('Can parse all-caps query values passed in', (done) => {
+        const cleanName = QueryOptions.getCleanName('GAME_NAME');
+        cleanName.should.equal('game_name');
+        done();
+      });
+
+      it('Returns null if an unexpected query param is passed in', (done) => {
+        expect(QueryOptions.getCleanName('4')).to.equal(null);
+        expect(QueryOptions.getCleanName('')).to.equal(null);
+        done();
+      });
+    });
+
+    describe('Allowed qualities', () => {
+      const optionSet = {
+        include_top_games: { type: 'boolean', default: false },
+        games_count: { type: 'number' },
+        game_id: { type: 'number', duplicate: true }
+      };
+
+      it('Lists the qualities a given parameter can have', (done) => {
+        const qualities = QueryOptions.getAllowedProperties(optionSet, 'include_top_games');
+        expect(qualities).to.have.property('type', 'boolean');
+        expect(qualities).to.have.property('default', false);
+        done();
+      });
+
+      it('returns null for a non-existent parameter', (done) => {
+        expect(QueryOptions.getAllowedProperties(optionSet, 'language')).to.equal(null);
+        expect(QueryOptions.getAllowedProperties(optionSet, 5)).to.equal(null);
+        expect(QueryOptions.getAllowedProperties(optionSet, null)).to.equal(null);
+        done();
+      });
+    });
+
+    describe('Parameter name cleaning', () => {
+      it('Finds relevant parameters in the user-passed params', () => {
+        const names = QueryOptions.getMatchingNames(
+          { include_TOP: true, include_top: [true, 0], INCLUDE_TOP: false, include_t: false },
+          'include_top'
+        );
+
+        names.should.be.an('array').of.length(3);
+      });
+
+      it('Returns null if no relevant parameters were passed', () => {
+        const names = QueryOptions.getMatchingNames(
+          {
+            include_t: true,
+            games_first: 20,
+            games_before: 5,
+            game_name: 'Rimworld'
+          },
+          'include_top'
+        );
+
+        expect(names).to.equal(null);
+      });
+    });
+
+    describe('Parameter value cleaning', () => {
+      const optionSet = {
+        include_top_games: { type: 'boolean', default: false },
+        games_count: { type: 'number' },
+        game_id: { type: 'number', duplicate: true }
+      };
+
+      const params = {
+        INCLUDE_TOP_GAMES: false,
+        games_count: 5,
+        game_id: [1, 20, 500],
+        GAME_ID: 63
+      };
+
+      const vals = QueryOptions.getPassedVals(optionSet, ['game_id', 'GAME_ID'], params);
+      expect(vals)
+        .to.be.an('array')
+        .with.lengthOf(4);
+    });
+
+    });
   });
 });
