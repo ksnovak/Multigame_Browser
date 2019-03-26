67febf14b68d5fc7e8e1b11d20ac1e400fdf3628
Kevin Novak <knovak8@gmail.com>
2019-03-25 22:09:51 -0700

    Add placeholder for loading images
    #77

--- src/client/Directory/StreamCell.js
+++ src/client/Directory/StreamCell.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 // import './Directory.css';
-import './StreamCell.css';
-import PropTypes from 'prop-types';
+import "./StreamCell.css";
+import PropTypes from "prop-types";
 
 const StreamWidth = 230;
 const StreamAspectRatio = 1.75;
@@ -20,7 +20,7 @@ export default class StreamCell extends Component {
 
   getStreamThumbnail(name) {
     return this.getThumbnail(
-      'https://static-cdn.jtvnw.net/previews-ttv/live_user_',
+      "https://static-cdn.jtvnw.net/previews-ttv/live_user_",
       name,
       StreamWidth,
       StreamAspectRatio
@@ -29,7 +29,7 @@ export default class StreamCell extends Component {
 
   getGameThumbnail(game) {
     return this.getThumbnail(
-      'https://static-cdn.jtvnw.net/ttv-boxart/',
+      "https://static-cdn.jtvnw.net/ttv-boxart/",
       game,
       GameWidth,
       GameAspectRatio
@@ -43,7 +43,10 @@ export default class StreamCell extends Component {
       <div className="streamCell">
         <div className="streamThumbnail">
           <a href={`https://twitch.tv/${stream.name}`}>
-            <img src={this.getStreamThumbnail(stream.name)} alt={`${stream.name}'s thumbnail`} />
+            <img
+              src={this.getStreamThumbnail(stream.name)}
+              alt={`${stream.name}'s thumbnail`}
+            />
           </a>
         </div>
 
@@ -53,7 +56,11 @@ export default class StreamCell extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            <img src={this.getGameThumbnail(game.name)} className="gameThumbnail" alt={game.name} />
+            <img
+              src={this.getGameThumbnail(game.name)}
+              className="gameThumbnail"
+              alt={game.name}
+            />
           </a>
         </div>
 
@@ -63,7 +70,9 @@ export default class StreamCell extends Component {
           <span className="viewerCount">{stream.viewers} viewers</span>
         </div>
 
-        <span className="streamGame">{game ? ` playing ${game.name}` : ''}</span>
+        <span className="streamGame">
+          {game ? ` playing ${game.name}` : ""}
+        </span>
         <span className="streamTitle">{stream.title}</span>
       </div>
     );
